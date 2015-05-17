fa7a0e246a3ba3b7c5be115cd0c20b486675932e
yunhai <yunhaihuang@gmail.com>
2015-05-17 20:53:01 +0700

    send mail

--- modules/cores/pages/pages_controler_public.php
+++ modules/cores/pages/pages_controler_public.php
@@ -10,7 +10,7 @@ class pages_controler_public extends VSControl_public {
         }
         unset ( $_SESSION ['active'] );
     }
-    
+
     /**
      *
      * @var pages
@@ -18,12 +18,12 @@ class pages_controler_public extends VSControl_public {
     protected $model;
     function auto_run() {
         global $bw;
-        
+
         switch ($bw->input ['action']) {
             case $this->modelName . '_detail' :
                 $this->showDetail ( $bw->input [2] );
                 break;
-            
+
             case $this->modelName . '_category' :
                 $this->showCategory ( $bw->input [2] );
                 break;
@@ -53,16 +53,15 @@ class pages_controler_public extends VSControl_public {
         if (! $category || $bw->input [1]) {
             $vsPrint->boink_it ( $bw->base_url . "404.html" );
         }
-        
-        
+
         if($bw->input['module'] == 'abouts') {
             $tmp = $category->getChildren();
             reset($tmp);
             $tmp = current($tmp);
-            
+
             return $vsPrint->boink_it($tmp->getCatUrl());
         }
-        
+
         $ids = VSFactory::getMenus ()->getChildrenIdInTree ( $category );
         $this->model->setCondition ( "status>0 and catId in ($ids)" );
         $this->model->setOrder ( "`index` desc,id desc" );
@@ -72,12 +71,12 @@ class pages_controler_public extends VSControl_public {
         $option ['title'] = VSFactory::getLangs ()->getWords ( $bw->input [0] );
         $vsPrint->mainTitle = $vsPrint->pageTitle = $option ['title'];
         $option ['cate'] = $category->getChildren ();
-        
+
         $option ['idcate'] = 0;
         if ($bw->input ['cate']) {
             $option ['idcate'] = $bw->input ['cate'];
         }
-        
+
         $i = 1;
         foreach ( $option ['pageList'] as $value ) {
             if ($i <= 9) {
@@ -87,34 +86,33 @@ class pages_controler_public extends VSControl_public {
             }
             $i ++;
         }
-        
-        
+
         return $this->output = $this->getHtml ()->showDefault ( $option );
     }
-    
+
     function showCategory($catId) {
         global $bw, $vsPrint;
-        
+
         if($bw->input[0] == 'abouts') {
             return $this->_aboutCategory($catId);
         }
         $special = array('projects', 'abouts');
-        
+
         if (in_array($bw->input [0], $special)) {
-            
-            
+
+
             $idcate = $this->getIdFromUrl ( $catId );
-            
+
             $category2 = VSFactory::getMenus ()->getCategoryById ( $idcate );
-            
+
             $category = VSFactory::getMenus ()->getCategoryGroup ( $bw->input [0] );
             $option ['cate_list'] = VSFactory::getMenus ()->getCategoryGroup ( $bw->input [0] )->getChildren ();
-            
+
             if (! $category2) {
                 $vsPrint->boink_it ( $bw->base_url . "404.html" );
             }
-            
-            
+
+
             $ids = VSFactory::getMenus ()->getChildrenIdInTree ( $category );
             $this->model->setCondition ( "status>0 and catId in ($ids)" );
             $this->model->setOrder ( "`index` desc,id desc" );
@@ -124,9 +122,9 @@ class pages_controler_public extends VSControl_public {
             $option ['title'] = VSFactory::getLangs ()->getWords ( $bw->input [0] );
             $vsPrint->mainTitle = $vsPrint->pageTitle = $option ['title'];
             $option ['cate'] = $category->getChildren ();
-            
+
             $option ['idcate'] = $category2->getId ();
-            
+
             $i = 1;
             foreach ( $option ['pageList'] as $value ) {
                 if ($i <= 9) {
@@ -136,12 +134,12 @@ class pages_controler_public extends VSControl_public {
                 }
                 $i ++;
             }
-            
+
             if($bw->input [0] == 'abouts') {
                 $option['category'] = $option ['cate'];
                 return $this->output = $this->getHtml ()->showAboutDefault ( $option );
             }
-            
+
             return $this->output = $this->getHtml ()->showDefault ( $option );
         } else {
             $category = VSFactory::getMenus ()->getCategoryGroup ( $bw->input [0] );
@@ -151,20 +149,20 @@ class pages_controler_public extends VSControl_public {
                 $vsPrint->boink_it ( $bw->base_url . "404.html" );
             }
             $_SESSION ['active'] = $category->getId ();
-            
+
             $ids = VSFactory::getMenus ()->getChildrenIdInTree ( $category );
             $this->model->setCondition ( "status>0 and catId in ({$idcate})" );
-            
+
             $this->model->setOrder ( "`index` desc,id desc" );
             $option = $this->model->getPageList ( $bw->input [0] . "/" . $bw->input [1] . "/" . $bw->input [2], 3, VSFactory::getSettings ()->getSystemKey ( $bw->input [0] . '_paging_limit', 12 ) );
-            
+
             $option ['title'] = $category->getTitle ();
             $vsPrint->mainTitle = $vsPrint->pageTitle = $option ['title'];
-            
+
             $option ['breakcrum'] = $this->createBreakCrum ( VSFactory::getMenus ()->getCategoryById ( $idcate ) );
             $option ['obj'] = $category;
             if ($option ['pageList'] and in_array ( $bw->input [0], array (
-                            'proservicer' 
+                            'proservicer'
             ) )) {
                 $obj = current ( $option ['pageList'] );
                 $vsPrint->boink_it ( $obj->getUrl ( $bw->input [0] ) );
@@ -177,33 +175,33 @@ class pages_controler_public extends VSControl_public {
                     $value->active = "";
                 }
             }
-            
+
             return $this->output = $this->getHtml ()->showDefault ( $option );
         }
     }
-    
+
     function _aboutCategory($catId) {
         global $bw, $vsPrint;
-        
+
         $idcate = $this->getIdFromUrl ( $catId );
         $category2 = VSFactory::getMenus ()->getCategoryById ( $idcate );
 
         if (! $category2) {
             $vsPrint->boink_it ( $bw->base_url . "404.html" );
         }
-        
+
         $ids = VSFactory::getMenus ()->getChildrenIdInTree ( $category2 );
         $this->model->setCondition ( "status>0 and catId in ($ids)" );
         $this->model->setOrder ( "`index` desc,id desc" );
-        
+
         $option = $this->model->getPageList ( $bw->input [0] . "/" . $bw->input [1] . "/" . $bw->input [2], 3, VSFactory::getSettings ()->getSystemKey ( $bw->input [0] . '_paging_public_limit', 12 ) );
         $option ['breakcrum'] = $this->createBreakCrum ( VSFactory::getMenus ()->getCategoryById ( $idcate ) );
         $vsPrint->mainTitle = $vsPrint->pageTitle = $option ['title'] = VSFactory::getLangs ()->getWords ( $bw->input [0] );
-        
+
         $option ['category'] = VSFactory::getMenus ()->getCategoryGroup ( $bw->input [0] )->getChildren();
-       
+
         $option ['idcate'] = $category2->getId ();
-       
+
         $i = 1;
         foreach ( $option ['pageList'] as $value ) {
             if ($i <= 9) {
@@ -213,12 +211,12 @@ class pages_controler_public extends VSControl_public {
             }
             $i ++;
         }
-        
+
         return $this->output = $this->getHtml ()->showAboutDefault ( $option );
-            
-    }    
-    
-    
+
+    }
+
+
     function showTag($tagId) {
         global $bw, $vsPrint;
         require_once (CORE_PATH . 'tags/tags.php');
@@ -230,14 +228,14 @@ class pages_controler_public extends VSControl_public {
         $this->model->setCondition ( "status >0 and catId in ({$ids}) and id in ({$id})" );
         // $this->model->setOrder("`index` desc,id desc");
         $option = $this->model->getPageList ( $bw->input [0] . "/" . $bw->input [1] . "/" . $bw->input [2], 3, VSFactory::getSettings ()->getSystemKey ( $bw->input [0] . '_paging_limit', 12 ) );
-        
+
         $option ['title'] = $category->getTitle ();
         $vsPrint->mainTitle = $vsPrint->pageTitle = $option ['title'];
         $option ['breakcrum'] = $this->createBreakCrum ( null );
         // $option['obj']=$category;
         $_SESSION ['active'] ['tag'] = $idtag;
         $option ['cate'] = VSFactory::getMenus ()->getCategoryGroup ( $bw->input [0] )->getChildren ();
-        
+
         return $this->output = $this->getHtml ()->showDefault ( $option );
     }
     function showDetail($objId, $option = array()) {
@@ -260,22 +258,22 @@ class pages_controler_public extends VSControl_public {
             }
         }
         $obj->createSeo ();
-        
+
         require_once CORE_PATH . 'gallerys/gallerys.php';
         $galerys = new gallerys ();
         $option ['files_list'] = $galerys->getAlbumByCode ( $bw->input [0] . "_" . $obj->getId () );
         $option ['title'] = $option ['cate_obj']->getTitle ();
-        
+
         require_once (CORE_PATH . 'tags/tags.php');
         $tags = new tags ();
         $tags->setCondition ( "id IN (SELECT tagId FROM vsf_tagcontent WHERE module ='{$bw->input[0]}' and contentId={$obj->getId()})" );
         $option ['list'] = $tags->getObjectsByCondition ();
-        
+
         $this->output = $this->getHtml ()->showDetail ( $obj, $option );
     }
     function showSearch() {
         global $bw, $vsTemplate, $vsStd, $vsPrint;
-        
+
         $option ['breakcrum'] = $this->createBreakCrum ( null );
         $category = VSFactory::getMenus ()->getCategoryGroup ( $bw->input [0] );
         // echo 123; exit();
@@ -289,7 +287,7 @@ class pages_controler_public extends VSControl_public {
         if ($bw->input ['keyword']) {
             $condition .= " status >0  and  title like '%" . mysql_real_escape_string ( $bw->input ['keyword'] ) . "%'";
         }
-        
+
         $this->model->setCondition ( $condition );
         $this->model->setOrder ( "`index`,id desc" );
         $option ['pageList'] = $this->model->getObjectsByCondition ();
@@ -306,54 +304,53 @@ class pages_controler_public extends VSControl_public {
         else
             $option ['title'] = VSFactory::getLangs ()->getWords ( 'products_search_result', 'Kết quả tìm kiếm' );
         $vsPrint->mainTitle = $vsPrint->pageTitle = $option ['title'];
-        
+
         return $this->output = $this->getHtml ()->showSearch ( $option );
     }
     function showSendcontact($option = array()) {
         global $bw, $vsTemplate, $vsStd, $vsPrint;
-        
+
         $vsLang = VSFactory::getLangs ();
         $this->vsLang = VSFactory::getLangs ();
-        
+
         require_once CORE_PATH . 'contacts/contacts.php';
         $contacts = new contacts ();
-        
+
         $contacts->basicObject->setTitle ( $bw->input ['name_contacts'] );
         $contacts->basicObject->setName ( $bw->input ['name_contacts'] );
         $contacts->basicObject->setEmail ( $bw->input ['email_contacts'] );
         $contacts->basicObject->setContent ( $bw->input ['message_contacts'] );
         $contacts->insertObject ();
-        
-        $vsStd->requireFile ( LIBS_PATH . "Email.class.php", true );
-        $this->email = new Emailer ();
-        $this->email->setTo ( VSFactory::getSettings ()->getSystemKey ( "email_admin", "mclchuang@weicovina.com.vn", "configs" ) );
-        $time = VSFactory::getDateTime ()->getDate ( time (), "d/m/y h:i" );
-        
-        $from = empty ( $bw->input ['email_contacts'] ) ? VSFactory::getSettings ()->getSystemKey ( "email_admin", "mclchuang@weicovina.com.vn", "configs" ) : $bw->input ['email_contacts'];
-        $this->email->setFrom ( $from );
-        
-        $this->email->setSubject ( $this->vsLang->getWords ( "title_email_sender", "Liên hệ" ) . " | {$time}" );
-        
-        $content = <<<EOF
-					<p> Họ tên    	: {$bw->input['name_contacts']}<p>					
-					<p>	Email    	: {$bw->input['email_contacts']}<p>					
-					<p>	Nội dung	: {$bw->input['message_contacts']}<p>
+
+
+        $message = <<<EOF
+                    <p> Họ tên    	: {$bw->input['name_contacts']}<p>
+                    <p>	Email    	: {$bw->input['email_contacts']}<p>
+                    <p>	Nội dung	: {$bw->input['message_contacts']}<p>
 EOF;
-        
-        $this->email->setBody ( $content );
-        
-        $this->email->sendMail ();
-        
+
+        $recipient = VSFactory::getSettings()->getSystemKey("email_admin", "mclchuang@gmail.com", "configs");
+        $recipientAlias = $this->model->obj->getTitle();
+
+        $from = $bw->input ['email_contacts'];
+        $fromName = $bw->input ['email_contacts'];
+
+        $subject = $this->vsLang->getWords("title_email_sender", "Liên hệ");
+
+        $this->sendEmail(compact('message', 'recipient', 'recipientAlias', 'from', 'fromName', 'subject'));
+
         $flag = 1;
         $message = $this->vsLang->getWords ( "contact_successfully_send", 'Cám ơn quý khách đã gửi liên hệ cho chúng tôi' );
-        
+
         header ( 'Content-Type: application/json' );
         $this->output = json_encode ( array (
                         'flag' => $flag,
-                        'message' => $message 
+                        'message' => $message
         ) );
     }
-    
+
+
+
     /**
      *
      * @param
@@ -376,7 +373,7 @@ EOF;
     /**
      *
      * Enter description here ...
-     * 
+     *
      * @var skin_pages
      */
     public $html;
--- sources/libs/boards/Control.board.php
+++ sources/libs/boards/Control.board.php
@@ -1,42 +1,97 @@
 <?php
 class VSControl {
-	protected $output;
-	function  auto_run() {
-		return;
-	}
-	function setOutput($out) {
-			return $this->output = $out;
-		}
-		
-		function getOutput() {
-			return $this->output;
-		}
-	function exitDenyAccess($error=''){
-		return $this->output=$error?$error:VSFactory::getLangs()->getWords('exitDenyAccess','Access denied!');
-	}
-	/**
-	 * @return skins_board
-	 */
-	public function getHtml() {
-		return $this->html;
-	}
+    protected $output;
+    function  auto_run() {
+        return;
+    }
+    function setOutput($out) {
+            return $this->output = $out;
+        }
 
-	/**
-	 * @param $html string file name
-	 */
-	public function setHtml($html) {
-		$this->html = $html;
-	}
-	function getIdFromUrl($url,$sep="-"){
-		$url=str_replace(".html", "", $url);
-		$url=explode($sep, $url);
-		return intval($url[count($url)-1]);
-	}
-	function lastModifyChange(){
-		if(!LAST_MODIFY_FILE) return false;
-		$fp = fopen(LAST_MODIFY_FILE, 'w');
-		fwrite($fp, 'modified');
-		fclose($fp);
-	}
+        function getOutput() {
+            return $this->output;
+        }
+    function exitDenyAccess($error=''){
+        return $this->output=$error?$error:VSFactory::getLangs()->getWords('exitDenyAccess','Access denied!');
+    }
+    /**
+     * @return skins_board
+     */
+    public function getHtml() {
+        return $this->html;
+    }
+
+    /**
+     * @param $html string file name
+     */
+    public function setHtml($html) {
+        $this->html = $html;
+    }
+    function getIdFromUrl($url,$sep="-"){
+        $url=str_replace(".html", "", $url);
+        $url=explode($sep, $url);
+        return intval($url[count($url)-1]);
+    }
+    function lastModifyChange(){
+        if(!LAST_MODIFY_FILE) return false;
+        $fp = fopen(LAST_MODIFY_FILE, 'w');
+        fwrite($fp, 'modified');
+        fclose($fp);
+    }
+
+    function sendEmail($option) {
+        global $vsStd, $bw;
+
+        extract($option);
+
+        $vsStd->requireFile ( UTILS_PATH . "mailer/PHPMailerAutoload.php", true );
+        $mail = new PHPMailer();
+
+        // set mailer to use SMTP
+        $mail->IsSMTP();
+
+        // As this email.php script lives on the same server as our email server
+        // we are setting the HOST to localhost
+        $mail->Host = "localhost";  // specify main and backup server
+
+        $mail->SMTPAuth = true;     // turn on SMTP authentication
+
+        // When sending email using PHPMailer, you need to send from a valid email address
+        // In this case, we setup a test email account with the following credentials:
+        // email: contact@domain.com
+            // pass: password
+
+        $mail->Username = $bw->vars['email_smtp_user'];  // SMTP username
+        $mail->Password = $bw->vars['email_smtp_password']; // SMTP password
+
+        // $email is the user's email address the specified
+        // on our contact us page. We set this variable at
+        // the top of this page with:
+        // $email = $_REQUEST['email'] ;
+        $mail->From     = $bw->vars['email_smtp_user'];
+        $mail->FromName = $fromName;
+
+        // below we want to set the email address we will be sending our email to.
+        $mail->AddAddress($recipient, $recipientAlias);
+
+        // set word wrap to 50 characters
+        $mail->WordWrap = 5000;
+        // set email format to HTML
+        $mail->IsHTML(true);
+
+        $mail->Subject = $subject;
+
+        // $message is the user's message they typed in
+        // on our contact us page. We set this variable at
+        // the top of this page with:
+        // $message = $_REQUEST['message'] ;
+        $mail->Body    = $message;
+        $mail->AltBody = $message;
+
+        return $mail->Send();
+    }
+
+
+
 }
 ?>
