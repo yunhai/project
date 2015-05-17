362cab8c46a5dddfff8617d9a249dd7a919202e9
yunhai <yunhaihuang@gmail.com>
2015-05-17 22:23:37 +0700

    cache

---
+++ cache/admin.setting.php
@@ -0,0 +1,485 @@
+<?php
+$arraySetting = array (
+  'contact_form_name' => '1',
+  'news_paging_public_limit' => '30',
+  'services_paging_limit_other' => '12',
+  'email_admin' => 'mclchuang@gmail.com',
+  'projects_paging_limit_other' => '12',
+  'projects_value' => '1',
+  'projects_status' => '1',
+  'projects_limit_files' => '0',
+  'album_image_size_height_projects' => '',
+  'album_image_size_width_projects' => '',
+  'recruitments_paging_limit_other' => '12',
+  'projects_paging_public_limit' => '12',
+  'abouts_paging_limit' => '12',
+  'recruitments_paging_public_limit' => '30',
+  'public_cleanurl' => '1',
+  'admins_admins_phone' => '0',
+  'admins_admins_address' => '0',
+  'admins_admins_email' => '0',
+  'admins_admins_content' => '0',
+  'admins_admins_intro' => '0',
+  'admins_admins_image_size' => '0',
+  'admins_admins_image' => '0',
+  'abouts_paging_limit_other' => '12',
+  'abouts_paging_public_limit' => '30',
+  'contacts_pcontacts_google_map' => '0',
+  'contacts_pcontacts_intro' => '0',
+  'contacts_pcontacts_image_width' => '',
+  'contacts_pcontacts_comment' => '0',
+  'contacts_pcontacts_album' => '0',
+  'contacts_pcontacts_video' => '0',
+  'contacts_pcontacts_postdate' => '0',
+  'contacts_pcontacts_title' => '1',
+  'contacts_pcontacts_image_field' => '0',
+  'contacts_pcontacts_id' => '1',
+  'contacts_pcontacts_index' => '0',
+  'contacts_pcontacts_status_highlight' => '0',
+  'contacts_pcontacts_status_trash_action' => '0',
+  'contacts_pcontacts_status_home' => '0',
+  'contacts_pcontacts_button_visible' => '1',
+  'contacts_pcontacts_button_disable' => '1',
+  'contacts_pcontacts_button_delete' => '1',
+  'contacts_pcontacts_button_add' => '1',
+  'contacts_pcontacts_status' => '1',
+  'contacts_pcontacts_category_list' => '0',
+  'contacts_pcontacts_search_form' => '1',
+  'pcontacts_admin_paging_limit' => '20',
+  'contacts_contacts_index' => '1',
+  'contacts_contacts_status_highlight' => '0',
+  'contacts_contacts_status_trash_action' => '0',
+  'contacts_contacts_status_home' => '0',
+  'contacts_contacts_button_visible' => '1',
+  'contacts_contacts_button_disable' => '1',
+  'contacts_contacts_button_delete' => '1',
+  'contacts_contacts_button_add' => '1',
+  'contacts_contacts_search_form' => '1',
+  'contacts_paging_limit' => '20',
+  'contacts_settings_tab' => '0',
+  'contacts_category_list' => '0',
+  'recruitments_pages_tag' => '0',
+  'recruitments_pages_content' => '1',
+  'recruitments_pages_editor_intro' => '0',
+  'recruitments_pages_intro' => '0',
+  'recruitments_pages_image_width' => '',
+  'recruitments_pages_code' => '0',
+  'recruitments_pages_website' => '0',
+  'seos_seos_comment' => '0',
+  'seos_seos_album' => '0',
+  'seos_seos_video' => '0',
+  'seos_seos_postdate' => '1',
+  'seos_seos_image_field' => '0',
+  'seos_seos_index' => '1',
+  'seos_seos_status_home' => '0',
+  'seos_seos_button_visible' => '1',
+  'seos_seos_button_disable' => '1',
+  'seos_seos_button_delete' => '1',
+  'seos_seos_button_add' => '1',
+  'seos_seos_status' => '1',
+  'seos_seos_search_form' => '1',
+  'seos_admin_paging_limit' => '20',
+  'seos_category_list' => '0',
+  '_image_size' => '',
+  'langs_langs_search_form' => '1',
+  'langs_langs_button_update' => '1',
+  'langs_langs_button_add' => '1',
+  'langs_langs_comment' => '0',
+  'langs_langs_album' => '0',
+  'langs_langs_video' => '0',
+  'langs_langs_button_delete' => '1',
+  'langs_paging_limit' => '20',
+  'langs_category_list' => '0',
+  'langs_vslang_tab' => '0',
+  'recruitments_pages_comment' => '0',
+  'recruitments_pages_album' => '0',
+  'recruitments_pages_video' => '0',
+  'recruitments_pages_postdate' => '1',
+  'recruitments_pages_title' => '1',
+  'recruitments_pages_image_field' => '0',
+  'recruitments_pages_id' => '1',
+  'recruitments_pages_index' => '1',
+  'recruitments_pages_status_highlight' => '0',
+  'recruitments_pages_status_trash_action' => '0',
+  'recruitments_pages_status_home' => '0',
+  'recruitments_pages_button_visible' => '1',
+  'recruitments_pages_button_disable' => '1',
+  'recruitments_pages_button_delete' => '1',
+  'recruitments_pages_button_add' => '1',
+  'recruitments_pages_status' => '1',
+  'recruitments_pages_category_list' => '0',
+  'recruitments_pages_search_form' => '1',
+  'recruitments_category_list' => '0',
+  'services_paging_public_limit' => '30',
+  'home_keywords' => '',
+  'home_description' => '',
+  'home_title' => '',
+  'services_pages_tag' => '0',
+  'services_pages_content' => '1',
+  'services_pages_editor_intro' => '0',
+  'services_pages_intro' => '1',
+  'services_pages_image_width' => '',
+  'services_pages_code' => '0',
+  'services_pages_website' => '0',
+  'services_pages_comment' => '0',
+  'services_pages_album' => '0',
+  'services_pages_video' => '0',
+  'services_pages_postdate' => '1',
+  'services_pages_title' => '1',
+  'services_pages_image_field' => '0',
+  'services_pages_id' => '1',
+  'services_pages_index' => '1',
+  'services_pages_status_highlight' => '0',
+  'services_pages_status_trash_action' => '0',
+  'services_pages_status_home' => '1',
+  'services_pages_button_visible' => '1',
+  'services_pages_button_disable' => '1',
+  'services_pages_button_delete' => '1',
+  'services_pages_button_add' => '1',
+  'services_pages_status' => '1',
+  'services_pages_category_list' => '0',
+  'services_pages_search_form' => '1',
+  'services_category_list' => '0',
+  'projects_cat_document' => '0',
+  'projects_cat_file' => '0',
+  'projects_cat_index' => '1',
+  'projects_cat_intro' => '0',
+  'projects_cat_desc' => '0',
+  'projects_cat_value' => '0',
+  'projects_cat_status' => '0',
+  'projects_cat_title' => '1',
+  'projects_cat_intro_editor_type' => '0',
+  'projects_arrayimg' => '0',
+  'projects_delete_category' => '1',
+  'projects_edit_category' => '1',
+  'abouts_value' => '1',
+  'abouts_status' => '1',
+  'files_category_list' => '0',
+  'abouts_pages_tag' => '0',
+  'abouts_pages_content' => '1',
+  'abouts_pages_editor_intro' => '0',
+  'abouts_pages_intro' => '0',
+  'abouts_pages_image_width' => '',
+  'abouts_pages_code' => '0',
+  'abouts_pages_website' => '0',
+  'abouts_cat_document' => '0',
+  'abouts_cat_file' => '0',
+  'abouts_cat_index' => '1',
+  'abouts_cat_intro' => '0',
+  'abouts_cat_desc' => '0',
+  'abouts_cat_value' => '0',
+  'abouts_cat_status' => '0',
+  'abouts_cat_title' => '1',
+  'abouts_cat_intro_editor_type' => '0',
+  'abouts_arrayimg' => '0',
+  'abouts_delete_category' => '1',
+  'abouts_edit_category' => '1',
+  'abouts_limit_files' => '0',
+  'album_image_size_height_abouts' => '',
+  'album_image_size_width_abouts' => '',
+  'gallerys_category_list' => '0',
+  'projects_pages_tag' => '0',
+  'projects_pages_content' => '1',
+  'projects_pages_editor_intro' => '0',
+  'projects_pages_intro' => '1',
+  'projects_pages_image_height' => '',
+  'projects_pages_image_width' => '',
+  'projects_pages_code' => '0',
+  'projects_pages_website' => '0',
+  'projects_pages_comment' => '0',
+  'projects_pages_album' => '1',
+  'projects_pages_video' => '0',
+  'projects_pages_postdate' => '1',
+  'projects_pages_title' => '1',
+  'projects_pages_image_field' => '1',
+  'projects_pages_id' => '1',
+  'projects_pages_index' => '1',
+  'projects_pages_status_highlight' => '0',
+  'projects_pages_status_trash_action' => '0',
+  'projects_pages_status_home' => '1',
+  'projects_pages_button_visible' => '1',
+  'projects_pages_button_disable' => '1',
+  'projects_pages_button_delete' => '1',
+  'projects_pages_button_add' => '1',
+  'projects_pages_status' => '1',
+  'projects_pages_category_list' => '1',
+  'projects_pages_search_form' => '1',
+  'projects_category_list' => '1',
+  'admins_admins_comment' => '0',
+  'admins_admins_album' => '0',
+  'admins_admins_video' => '0',
+  'admins_admins_name' => '1',
+  'admins_admins_status_home' => '0',
+  'admins_admins_button_visible' => '1',
+  'admins_admins_button_disable' => '1',
+  'admins_admins_button_delete' => '1',
+  'admins_admins_button_add' => '1',
+  'admins_admins_status' => '1',
+  'admins_admins_category_list' => '0',
+  'admins_admins_search_form' => '1',
+  'admins_admin_paging_limit' => '20',
+  'admins_settings_tab' => '0',
+  'admins_admins_group_tab' => '0',
+  'settings_settings_editor_intro' => '0',
+  'settings_settings_intro' => '1',
+  'settings_settings_index' => '1',
+  'settings_settings_status_home' => '0',
+  'settings_settings_status' => '1',
+  'settings_settings_value' => '1',
+  'settings_settings_key' => '1',
+  'settings_category_list' => '0',
+  'settings_settings_category_list' => '0',
+  'settings_settings_search_form' => '0',
+  'settings_paging_limit' => '20',
+  'abouts_pages_comment' => '0',
+  'abouts_pages_album' => '1',
+  'abouts_pages_video' => '0',
+  'abouts_pages_postdate' => '1',
+  'abouts_pages_title' => '1',
+  'abouts_pages_image_field' => '0',
+  'abouts_pages_id' => '1',
+  'abouts_pages_index' => '1',
+  'abouts_pages_status_highlight' => '0',
+  'abouts_pages_status_trash_action' => '0',
+  'abouts_pages_status_home' => '0',
+  'abouts_pages_button_visible' => '1',
+  'abouts_pages_button_disable' => '1',
+  'abouts_pages_button_delete' => '1',
+  'abouts_pages_button_add' => '1',
+  'abouts_pages_status' => '1',
+  'abouts_pages_category_list' => '1',
+  'abouts_pages_search_form' => '1',
+  'pages_admin_paging_limit' => '20',
+  'global_meta_descriptions' => '1',
+  'global_meta_keywords' => '1',
+  'abouts_category_list' => '1',
+  'global_websitename' => 'Weicovina',
+  'enable_gzip' => '1',
+  'public_menu_cache' => '1',
+  'use_cache_skin_wrapper' => '0',
+  'admin_multi_lang' => '0',
+  'user_multi_lang' => '0',
+  'global_server_timezone' => '7',
+  'admin_frontpage' => 'projects',
+  'header_redirect' => '',
+  'admin_timeout' => '1000',
+  'mail_wrap_brackets' => '0',
+  'email_smtp_port' => '',
+  'email_smtp_host' => '',
+  'email_method' => '',
+  'email_smtp_password' => 'noreply@weicovina.com.vn',
+  'email_smtp_user' => 'noreply@weicovina.com.vn',
+  'global_systememail' => '',
+  'setting_group' => 
+  array (
+    'abouts_pages_index' => 
+    array (
+      'abouts_pages_list' => '1',
+      'abouts_pages_form' => '1',
+    ),
+    'abouts_pages_id' => 
+    array (
+      'abouts_pages_list' => '1',
+    ),
+    'abouts_pages_image_field' => 
+    array (
+      'abouts_pages_list' => '1',
+      'abouts_pages_form' => '1',
+    ),
+    'abouts_pages_title' => 
+    array (
+      'abouts_pages_list' => '1',
+    ),
+    'abouts_pages_category_list' => 
+    array (
+      'abouts_pages_list' => '1',
+      'abouts_pages_form' => '1',
+    ),
+    'abouts_pages_status' => 
+    array (
+      'abouts_pages_list' => '1',
+      'abouts_pages_form' => '1',
+    ),
+    'abouts_pages_postdate' => 
+    array (
+      'abouts_pages_list' => '1',
+    ),
+    'projects_pages_index' => 
+    array (
+      'projects_pages_list' => '1',
+      'projects_pages_form' => '1',
+    ),
+    'projects_pages_id' => 
+    array (
+      'projects_pages_list' => '1',
+    ),
+    'projects_pages_image_field' => 
+    array (
+      'projects_pages_list' => '1',
+      'projects_pages_form' => '1',
+    ),
+    'projects_pages_title' => 
+    array (
+      'projects_pages_list' => '1',
+    ),
+    'projects_pages_category_list' => 
+    array (
+      'projects_pages_list' => '1',
+      'projects_pages_form' => '1',
+    ),
+    'projects_pages_status' => 
+    array (
+      'projects_pages_list' => '1',
+      'projects_pages_form' => '1',
+    ),
+    'projects_pages_postdate' => 
+    array (
+      'projects_pages_list' => '1',
+    ),
+    'projects_pages_code' => 
+    array (
+      'projects_pages_form' => '0',
+    ),
+    'projects_pages_intro' => 
+    array (
+      'projects_pages_form' => '1',
+    ),
+    'projects_pages_content' => 
+    array (
+      'projects_pages_form' => '1',
+    ),
+    'abouts_pages_code' => 
+    array (
+      'abouts_pages_form' => '0',
+    ),
+    'abouts_pages_intro' => 
+    array (
+      'abouts_pages_form' => '1',
+    ),
+    'abouts_pages_content' => 
+    array (
+      'abouts_pages_form' => '1',
+    ),
+    'services_pages_index' => 
+    array (
+      'services_pages_list' => '1',
+      'services_pages_form' => '1',
+    ),
+    'services_pages_id' => 
+    array (
+      'services_pages_list' => '1',
+    ),
+    'services_pages_image_field' => 
+    array (
+      'services_pages_list' => '1',
+      'services_pages_form' => '1',
+    ),
+    'services_pages_title' => 
+    array (
+      'services_pages_list' => '1',
+    ),
+    'services_pages_category_list' => 
+    array (
+      'services_pages_list' => '0',
+      'services_pages_form' => '0',
+    ),
+    'services_pages_status' => 
+    array (
+      'services_pages_list' => '1',
+      'services_pages_form' => '1',
+    ),
+    'services_pages_postdate' => 
+    array (
+      'services_pages_list' => '1',
+    ),
+    'services_pages_code' => 
+    array (
+      'services_pages_form' => '0',
+    ),
+    'services_pages_intro' => 
+    array (
+      'services_pages_form' => '1',
+    ),
+    'services_pages_content' => 
+    array (
+      'services_pages_form' => '1',
+    ),
+    'recruitments_pages_index' => 
+    array (
+      'recruitments_pages_list' => '1',
+      'recruitments_pages_form' => '1',
+    ),
+    'recruitments_pages_id' => 
+    array (
+      'recruitments_pages_list' => '1',
+    ),
+    'recruitments_pages_image_field' => 
+    array (
+      'recruitments_pages_list' => '1',
+      'recruitments_pages_form' => '1',
+    ),
+    'recruitments_pages_title' => 
+    array (
+      'recruitments_pages_list' => '1',
+    ),
+    'recruitments_pages_category_list' => 
+    array (
+      'recruitments_pages_list' => '0',
+      'recruitments_pages_form' => '0',
+    ),
+    'recruitments_pages_status' => 
+    array (
+      'recruitments_pages_list' => '1',
+      'recruitments_pages_form' => '1',
+    ),
+    'recruitments_pages_postdate' => 
+    array (
+      'recruitments_pages_list' => '1',
+    ),
+    'recruitments_pages_code' => 
+    array (
+      'recruitments_pages_form' => '0',
+    ),
+    'recruitments_pages_intro' => 
+    array (
+      'recruitments_pages_form' => '1',
+    ),
+    'recruitments_pages_content' => 
+    array (
+      'recruitments_pages_form' => '1',
+    ),
+    'contacts_contacts_index' => 
+    array (
+      'contacts_contacts_list' => '1',
+    ),
+    'contacts_pcontacts_index' => 
+    array (
+      'contacts_pcontacts_list' => '1',
+    ),
+    'contacts_pcontacts_id' => 
+    array (
+      'contacts_pcontacts_list' => '1',
+    ),
+    'contacts_pcontacts_image_field' => 
+    array (
+      'contacts_pcontacts_list' => '1',
+    ),
+    'contacts_pcontacts_title' => 
+    array (
+      'contacts_pcontacts_list' => '1',
+    ),
+    'contacts_pcontacts_category_list' => 
+    array (
+      'contacts_pcontacts_list' => '0',
+    ),
+    'contacts_pcontacts_status' => 
+    array (
+      'contacts_pcontacts_list' => '1',
+    ),
+    'contacts_pcontacts_postdate' => 
+    array (
+      'contacts_pcontacts_list' => '1',
+    ),
+  ),
+);
+?>
---
+++ cache/skins/user/log.php
@@ -0,0 +1,40 @@
+<?php
+$skinslog=array (
+  'filemtime' => 
+  array (
+    './skins/user/finance/skin_addon.php' => 1418136070,
+    './skins/user/finance/skin_global.php' => 1418135198,
+    './skins/user/finance/skin_home.php' => 1418135810,
+    './skins/user/finance/skin_loitrang.php' => 1416059586,
+    './skins/user/finance/skin_objectpublic.php' => 1416048480,
+    './skins/user/finance/skin_pages.php' => 1418137698,
+    './skins/user/finance/skin_pcontacts.php' => 1406002772,
+    './skins/user/finance/skin_projects.php' => 1416633756,
+    './skins/user/finance/skin_services.php' => 1416633768,
+  ),
+  'getlastmod' => 
+  array (
+    './skins/user/finance/skin_addon.php' => 1376876796,
+    './skins/user/finance/skin_global.php' => 1376876796,
+    './skins/user/finance/skin_home.php' => 1376876796,
+    './skins/user/finance/skin_loitrang.php' => 1376876796,
+    './skins/user/finance/skin_objectpublic.php' => 1376876796,
+    './skins/user/finance/skin_pages.php' => 1376876796,
+    './skins/user/finance/skin_pcontacts.php' => 1376876796,
+    './skins/user/finance/skin_projects.php' => 1376876796,
+    './skins/user/finance/skin_services.php' => 1376876796,
+  ),
+  'filectime' => 
+  array (
+    './skins/user/finance/skin_addon.php' => 1431797614,
+    './skins/user/finance/skin_global.php' => 1431797614,
+    './skins/user/finance/skin_home.php' => 1431797614,
+    './skins/user/finance/skin_loitrang.php' => 1431797614,
+    './skins/user/finance/skin_objectpublic.php' => 1431797614,
+    './skins/user/finance/skin_pages.php' => 1431797614,
+    './skins/user/finance/skin_pcontacts.php' => 1431797614,
+    './skins/user/finance/skin_projects.php' => 1431797614,
+    './skins/user/finance/skin_services.php' => 1431797614,
+  ),
+)
+?>
